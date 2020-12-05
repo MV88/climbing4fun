2c1b55dc7f368506a5ec1fde8670dfc895f32d9d
mv88 <matteo.velludini@gmail.com>
2020-12-05 13:37:21 +0100

    chore: initial setup
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        climbing4fun
+      </h1>
+      <div class="links">
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/test/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <div class="links">
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
